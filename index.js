13660eda372c5d021e468d93cbf832183377364a
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-05-09 12:13:10 -0300

    feat: :sparkles: mais inquirer inputs, escrever options.json, func. limpar input

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-import { input, select } from '@inquirer/prompts';
+import { input, select, confirm } from '@inquirer/prompts';
 import puppeteer from 'puppeteer';
+import fs from 'fs';
 
 (async () => {
+
+    // check if options.json exists
+    if (fs.existsSync('options.json')) {
+        const options = fs.readFileSync('options.json');
+        const data = JSON.parse(options);
+        var json_username = data.username;
+        var json_rows = data.rows;
+        var json_columns = data.columns;
+        var json_padding = data.padding;
+        var json_background_color = data.background_color;
+    }
+
+    const username = await input({
+        message: 'Enter your last.fm username',
+        default: json_username ? json_username : 'renatocfrancisc',
+        validate: (value) => {
+            const pass = value.match(
+                /^[a-zA-Z0-9]+$/
+            );
+            if (pass) {
+                return true;
+            }
+        }
+    });
+
     const period = await select({
         message: 'Select the period of the chart',
         choices: [
             {
                 title: '7day',
                 value: '7day'
+
             },
             {
                 title: '1month',
@@ -31,6 +58,7 @@ import puppeteer from 'puppeteer';
             }
         ],
     });
+
     const size = await select({
         message: 'Select the size of the chart',
         choices: [
@@ -53,10 +81,10 @@ import puppeteer from 'puppeteer';
         ],
     });
 
-    if(size === '25') {
+    if (size === '25') {
         const rows = await input({
             message: 'Select the number of rows',
-            initial: '6',
+            default: json_rows ? json_rows : '6',
             validate: (value) => {
                 const pass = value.match(
                     /^[0-9]+$/
@@ -71,7 +99,7 @@ import puppeteer from 'puppeteer';
 
         const columns = await input({
             message: 'Select the number of columns',
-            initial: '6',
+            default: json_columns ? json_columns : '6',
             validate: (value) => {
                 const pass = value.match(
                     /^[0-9]+$/
@@ -86,6 +114,73 @@ import puppeteer from 'puppeteer';
 
         var size_arr = [String(rows), String(columns)];
     };
+
+    const padding = await input({
+        message: 'Select the padding of the chart',
+        default: json_padding ? json_padding : '2',
+        validate: (value) => {
+            const pass = value.match(
+                /^[0-9]+$/
+            );
+            if (pass) {
+                return true;
+            }
+        }
+    });
+
+    const opt_background_color = await confirm({
+        message: 'Do you want to change the background color?',
+        initial: true
+    });
+
+    if(opt_background_color){
+        const background_color = await input({
+            message: 'Enter the background color',
+            default: json_background_color ? json_background_color : '000000',
+            validate: (value) => {
+                const pass = value.match(
+                    /^[a-zA-Z0-9]+$/
+                );
+                if (pass) {
+                    return true;
+                }
+            }
+        });
+
+        var hex_color = '#' + background_color;
+        var back_color = background_color;
+
+    }
+
+    const album_titles = await confirm({
+        message: 'Do you want to display the album titles?',
+        initial: true
+    });
+
+    if(album_titles) {
+        const numbered = await confirm({
+            message: 'Do you want to display the album numbers?',
+            initial: true
+        });
+
+        const play_counts = await confirm({
+            message: 'Do you want to display the playcounts?',
+            initial: true
+        });
+
+        var album_titles_options = [numbered, play_counts];
+    }
+
+    // save options on a json file
+    const data = {
+        username: username,
+        rows: size_arr[0],
+        columns: size_arr[1],
+        padding: padding,
+        background_color: back_color
+    };
+    const jsonData = JSON.stringify(data);
+    fs.writeFileSync('options.json', jsonData, 'utf-8');
 
     const browser = await puppeteer.launch({
         headless: false,
@@ -103,7 +198,7 @@ import puppeteer from 'puppeteer';
     await page.click('#lastfm-button');
 
     await page.waitForSelector('#lastfm-username-field');
-    await page.type('#lastfm-username-field', 'renatocfrancisc', {
+    await page.type('#lastfm-username-field', username, {
         delay: 50
     });
 
@@ -128,16 +223,55 @@ import puppeteer from 'puppeteer';
     await page.select('#customizations > div > label:nth-child(1) > select', size);
 
     // rows / columns
-    if(size === '25') {
+    if (size === '25') {
         console.log('rows / columns...');
         await page.waitForSelector('#customizations > div > label:nth-child(3) > span > input[type=range]');
-    
+        await new Promise(r => setTimeout(r, 2000));
+
         const rangeInput1 = await page.$('#customizations > div > label:nth-child(3) > span > input[type=range]');
         await rangeInput1.type(String(size_arr[0]), { delay: 100 });
-    
+        await new Promise(r => setTimeout(r, 2000));
+
         const rangeInput2 = await page.$('#customizations > div > label:nth-child(5) > span > input[type=range]');
         await rangeInput2.type(String(size_arr[1]), { delay: 100 })
     };
+
+    // album titles
+    if(album_titles) {
+        console.log('album titles...');
+        await page.waitForSelector('#titled > input[type=checkbox]');
+        await page.click('#titled > input[type=checkbox]');
+
+        if(album_titles_options[0]) {
+            console.log('numbered...');
+            await page.waitForSelector('#numbered > input[type=checkbox]');
+            await page.click('#numbered > input[type=checkbox]');
+        }
+
+        if(album_titles_options[1]){
+            console.log('play counts...');
+            await page.waitForSelector('#playcounts > input[type=checkbox]');
+            await page.click('#playcounts > input[type=checkbox]');
+        }
+    }
+
+    // background color
+    if(opt_background_color){
+        console.log('background color...');
+        await page.waitForSelector('#customizations > div > label:nth-child(15) > input[type=search]');
+        await limparInput('#customizations > div > label:nth-child(15) > input[type=search]');
+        await page.type('#customizations > div > label:nth-child(15) > input[type=search]', hex_color, {
+            delay: 50
+        });
+    }
+
+    // padding
+    if(padding !== '2'){
+        console.log('padding...');
+        await page.waitForSelector('#customizations > div > label:nth-child(19) > span > input[type=range]');
+        const rangeInput3 = await page.$('#customizations > div > label:nth-child(19) > span > input[type=range]');
+        await rangeInput3.type(padding, { delay: 100 });
+    }
 
     console.log('rendering...');
     await page.waitForSelector('#buttons > form > button:nth-child(8)');
@@ -147,4 +281,13 @@ import puppeteer from 'puppeteer';
     await new Promise(r => setTimeout(r, 15000));
 
     await browser.close();
+    return null
+
+    async function limparInput(selector) {
+        await page.click(selector);
+        await page.keyboard.down('Control');
+        await page.keyboard.press('A');
+        await page.keyboard.up('Control');
+        await page.keyboard.press('Backspace');
+    }
 })();
